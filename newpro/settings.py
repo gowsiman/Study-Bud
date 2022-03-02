4dfc0f53a63a18cb4b62bc7781b24559fb91b0d4
gowsiman <gowsimanarg19@gmail.com>
2022-03-03 00:40:17 +0530

    deploy5

--- newpro/settings.py
+++ newpro/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,6 +131,14 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_ROOT = BASE_DIR/ 'static/images'
 # Default primary key field type
